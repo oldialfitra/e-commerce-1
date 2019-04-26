abdab32164aa8916fd7e946a48eb97c881010da1
Lalu Oldi Alfitra <oldia9@gmail.com>
2019-04-26 09:39:28 +0700

    Deploy

--- server/app.js
+++ server/app.js
@@ -7,7 +7,8 @@ const express = require('express'),
     cors = require('cors'),
     routerIndex = require('./routes/index')
 
-mongoose.connect(`mongodb://localhost:27017/${process.env.DBNAME}`, { useNewUrlParser: true })
+// mongoose.connect(`mongodb://localhost:27017/${process.env.DBNAME}`, { useNewUrlParser: true })
+mongoose.connect(`mongodb+srv://oldi:delete@miniwp-9mbzj.gcp.mongodb.net/e-commerce-new?retryWrites=true`, { useNewUrlParser: true })
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
